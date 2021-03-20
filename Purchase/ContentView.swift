5e17569638da0b09fd1816d198794cd5af82e267
isa yilmaz <jesus.codes@icloud.com>
2021-03-20 12:04:56 +0300

    Initial Commit

---
+++ Purchase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Purchase
+//
+//  Created by İsa Yılmaz on 20.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Purchase/PurchaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurchaseApp.swift
+//  Purchase
+//
+//  Created by İsa Yılmaz on 20.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct PurchaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
